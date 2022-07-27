2235e6191e2bfe1c95fe4498c14e4a077c2b2587
Mikołaj Pieczaba <mikolaj@pieczaba.me>
2022-07-27 16:55:16 +0200

    feat: add database migrations

--- db/db.go
+++ db/db.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
@@ -14,7 +17,31 @@ func Connect() *sqlx.DB {
 		log.Fatalln(err)
 	}
 
+	if err := migrateDB(db); err != nil {
+		log.Fatal(err)
+	}
+
 	log.Println("Successfully connected to the database")
 
 	return db
+}
+
+func migrateDB(db *sqlx.DB) error {
+	driver, err := postgres.WithInstance(db.DB, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://db/migrations",
+		"postgres", driver,
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,14 @@ module github.com/clutterpot/clutterpot
 go 1.18
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.6
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 )
